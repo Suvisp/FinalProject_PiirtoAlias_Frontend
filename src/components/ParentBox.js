87709794b0fc8ccf0b886175650d970a5593d231
Niina Siitari <siitari.niina@gmail.com>
2020-03-25 09:15:45 +0200

    Tehtiin pohjaalustus komponenteille ja laitettiin id:t

---
+++ src/components/ParentBox.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ParentBox extends Component {
+    render() {
+        return (
+            <div id="parentbox">
+                
+            </div>
+        )
+    }
+}
---
+++ src/components/SocketBox.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SocketBox extends Component {
+    render() {
+        return (
+            <div id="socketbox">
+                
+            </div>
+        )
+    }
+}
