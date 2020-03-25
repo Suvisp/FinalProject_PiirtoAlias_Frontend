935bc54fad7e510429f23b99096b0fa12ea9f619
Niina Siitari <siitari.niina@gmail.com>
2020-03-25 12:34:07 +0200

    Linkitetty komponentit toisiinsa ja tehty piirtoalustaan sketchi, jonka toimivuus pitää tarkistaa app.js juuresta

--- src/components/ParentBox.js
+++ src/components/ParentBox.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import SocketBox from './SocketBox'
+import SketchBox from './SketchBox'
 
 export default class ParentBox extends Component {
     render() {
         return (
             <div id="parentbox">
-                
+               <SocketBox />
+               <SketchBox />
             </div>
         )
     }
--- src/components/SocketBox.js
+++ src/components/SocketBox.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import Viestinlahetys from './ViestinLahetys'
+import Chatviesti from './ChatViesti'
+import ListausPelaajat from './ListausPelaajat'
 
 export default class SocketBox extends Component {
     render() {
         return (
             <div id="socketbox">
-                
+                <ListausPelaajat />
+                <Chatviesti />
+                <Viestinlahetys />
             </div>
         )
     }
