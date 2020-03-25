b61928ca419d16bb119bdc7073688e5a2a0ea716
Niina Siitari <siitari.niina@gmail.com>
2020-03-25 13:35:56 +0200

    Tehtiin piirtocanvas, joka on todistettavasti toimii. Poistettiin vanhempi versio.

--- src/components/ParentBox.js
+++ src/components/ParentBox.js
@@ -3,14 +3,13 @@ import SocketBox from './SocketBox'
 import SketchBox from './SketchBox'
 import ArvattavaSana from './ArvattavaSana';
 
-
 export default class ParentBox extends Component {
     render() {
         return (
             <div id="parentbox">
                <SocketBox />
                <SketchBox />
-            <ArvattavaSana />
+               <ArvattavaSana />
             </div>
         )
     }
--- src/components/Piirtoalusta.js
+++ src/components/Piirtoalusta.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react'
+import ReactDOM from "react-dom";
+import CanvasDraw from "react-canvas-draw";
 
+ReactDOM.render(<CanvasDraw />, document.getElementById("root"));
 export default class Piirtoalusta extends Component {
+    state = {
+        color: "#ffc600",
+        width: 400,
+        height: 400,
+        brushRadius: 10,
+        lazyRadius: 12
+      };
     render() {
         return (
             <div id="piirtoalusta">
-                <div id="toolbar">
-                    <div id="rad">
-                        Radius <span id="radval">10</span>
-                        <div id="decrad" class="radcontrol">-</div>
-                        <div id="incrad" class="radcontrol">+</div>
-                    </div>
-
-                    <div id="colors"></div>
-
-                    <div id="clear">Clear</div>
-                </div>
-                <canvas id="Canvas">
-                    browser does not support
-                </canvas>
-                <script type="text/javascript" src="./canvas/main.js"></script>
-                <script type="text/javascript" src="./canvas/radius.js"></script>
-                <script type="text/javascript" src="./canvas/colors.js"></script>
-                <script type="text/javascript" src="./canvas/save.js"></script>
-                <script type="text/javascript" src="./canvas/clear.js"></script>
+                <CanvasDraw />
             </div>
         )
     }
